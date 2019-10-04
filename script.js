07712071a4f062644fbc7c69d6f8e91f9e7ece25
Arnold Diep <tungd@uw.edu>
2019-10-04 15:23:15 -0700

    Able to get password generated inside textarea.

--- script.js
+++ script.js
@@ -12,25 +12,50 @@
 
 //Bind all inputs
 
-//Create a function that randomly pick one of the following chosen input.
+//Create a function that randomly pick one of the following chosen criteria.
 
 //Make a loop that runs until 10 characters are given.
 
+var generateEl = document.querySelector("#generate");
+
+var num = prompt("How many characters do you want to have? Pick between 8-128.")
 var confirmSpecial = confirm("Do you want to include special characters?");
-var confirmNumbers = confirm("Do you want to include numbers?");
+var confirmNumbers = confirm("Do you want to include numberic characters?");
 var confirmUpper = confirm("Do you want to include uppercase characters?");
 var confirmLower = confirm("Do you want to include lowercase ");
+if (confirmSpecial === false && confirmNumbers === false && confirmUpper === false && confirmLower === false) {
+    alert("Please choose at least one criteria.")
+}
 
-funtion generate(length = 10) {
-    var special = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+function generate(num) {
+    console.log("Generate call");
+    var special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
     var numbers = "0123456789";
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lower = "abcdefghijklmnopqrstuvwxyz";
-    var all = special + numbers + upper + lower;
+    var all = "";
+    if (confirmSpecial) {
+        all = all + special;
+    }
+    if (confirmNumbers) {
+        all = all + numbers;
+    }
+    if (confirmUpper) {
+        all = all + upper;
+    }
+    if (confirmLower) {
+        all = all + lower;
+    }
     var password = "";
-    for (i = 0; o < length; i++) {
+    for (i = 0; i <= num; i++) {
         var character = Math.floor(Math.random() * all.length);
         password += all.substring(character, character + 1);
     }
+    console.log(password);
+    var passwordEL = document.querySelector("#password");
+    passwordEL.innerHTML = password
     return password;
 }
+
+
+
